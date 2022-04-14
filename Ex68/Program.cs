88b04ed6804eaf826f5bf3dea0323f428117797d
Anastasiya <stasyaya@rambler.ru>
2022-04-14 21:10:01 +0300

    Правка 68 задачи

--- Ex68/Program.cs
+++ Ex68/Program.cs
@@ -1,7 +1,7 @@
 ﻿// SЗадача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(n,m) = 29
 
-int numberN = 2;
+int numberN = 3;
 int numberM = 3;
 
 //Recursion(numberM, numberN);
@@ -11,11 +11,11 @@ int Recursion(int M, int N)
 {
     if (M ==0) 
     { 
-        return N+1;
+        return N + 1;
     }
         else if (N == 0) 
             {
-                Recursion(M-1,1);
+                return Recursion(M-1,1);
             }
             else 
             {
